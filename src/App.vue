0fd78116961df2a556efd87d172006cb718970e4
campari-soda <campari-soda@gmail.com>
2022-06-12 15:49:07 +0900

    add addTodo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
+import MainTodo from './components/MainTodo.vue';
+import { ref } from 'vue';
+const todoRef = ref('');
 </script>
 
 <template>
   <div class="wrap">
     <TheHeader />
-    <main class="main">main</main>
+    <main class="main">
+      <MainTodo />
+    </main>
     <TheFooter />
   </div>
 </template>
